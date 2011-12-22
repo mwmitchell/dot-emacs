1efe89551e632558dcda44bdd9af268cff29baa0
Matt Mitchell <goodieboy@gmail.com>
2011-12-22 15:53:22 -0500

    initial init.el commit

---
+++ init.el
@@ -0,0 +1,62 @@
+(load-file "~/.emacs.d/my-paths.el")
+
+(set-default-font "-adobe-courier-medium-r-normal--16-180-75-75-m-110-iso8859-1")
+
+;; show line nums
+(global-linum-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages
+  '(starter-kit
+    starter-kit-ruby
+    starter-kit-lisp
+    starter-kit-eshell
+    maxframe
+    clojure-mode
+    midje-mode
+    color-theme
+    color-theme-wombat
+    color-theme-wombat+
+    color-theme-gruber-darker)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; maxframe
+(add-hook 'window-setup-hook 'maximize-frame t)
+(custom-set-variables
+ '(mf-display-padding-height 100)
+ '(mf-offset-y 0))
+
+;; color-theme
+(load "color-theme")
+(load "color-theme-gruber-darker")
+(load "color-theme-wombat")
+(load "color-theme-wombat+")
+(color-theme-gruber-darker)
+
+;; clojure stuff
+(add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
+(defun clojure-mode-untabify ()
+ (save-excursion
+   (goto-char (point-min))
+   (while (re-search-forward "[ \t]+$" nil t)
+     (delete-region (match-beginning 0) (match-end 0)))
+   (goto-char (point-min))
+   (if (search-forward "\t" nil t)
+       (untabify (1- (point)) (point-max))))
+ nil)
+(add-hook 'clojure-mode-hook
+          '(lambda ()
+             (define-key clojure-mode-map "\C-c\C-f" 'slime-eval-defun)))
+(add-hook 'clojure-mode-hook
+          '(lambda () (add-hook 'write-contents-hooks 'clojure-mode-untabify nil t)))
