801d29d818467223919a7f0f475b81dd1286cc45
Matt Mitchell <mmitchell@roomkey.com>
2013-07-11 09:33:46 -0400

    adjusted display padding

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mf-display-padding-height 100)
+ '(mf-display-padding-height 105)
  '(mf-offset-y 0))
 
 ;; TODO: bind this to a command... this shrinks the window down --
