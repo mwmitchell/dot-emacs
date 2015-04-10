5b5f6c6617e663991c3e5621b1595dd758374a0b
Matt Mitchell <goodieboy@gmail.com>
2015-04-10 11:15:14 -0400

    tmp. using snapshots over melpa-stable

--- init.el
+++ init.el
@@ -36,11 +36,11 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
-;; (add-to-list 'package-archives
-;;              '("melpa" . "http://melpa.milkbox.net/packages/"))
-
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+;;(add-to-list 'package-archives
+;;             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (package-initialize)
 
